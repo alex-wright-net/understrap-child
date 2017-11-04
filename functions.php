a0bf1d313e5fd342e3c3adeb8fdfbc6601d74aae
Alex Wright <alex@alexwright.net>
2017-11-04 08:02:19 -0700

    Add function to include ACF options page

--- functions.php
+++ functions.php
@@ -1,12 +1,12 @@
 <?php
 function understrap_remove_scripts() {
-    wp_dequeue_style( 'understrap-styles' );
-    wp_deregister_style( 'understrap-styles' );
+		wp_dequeue_style( 'understrap-styles' );
+		wp_deregister_style( 'understrap-styles' );
 
-    wp_dequeue_script( 'understrap-scripts' );
-    wp_deregister_script( 'understrap-scripts' );
+		wp_dequeue_script( 'understrap-scripts' );
+		wp_deregister_script( 'understrap-scripts' );
 
-    // Removes the parent themes stylesheet and scripts from inc/enqueue.php
+		// Removes the parent themes stylesheet and scripts from inc/enqueue.php
 }
 add_action( 'wp_enqueue_scripts', 'understrap_remove_scripts', 20 );
 
@@ -16,10 +16,10 @@ function theme_enqueue_styles() {
 	// Get the theme data
 	$the_theme = wp_get_theme();
 
-    wp_enqueue_style( 'child-understrap-styles', get_stylesheet_directory_uri() . '/css/style.min.css', array(), $the_theme->get( 'Version' ) );
-    wp_enqueue_script( 'jquery' );
+		wp_enqueue_style( 'child-understrap-styles', get_stylesheet_directory_uri() . '/css/style.min.css', array(), $the_theme->get( 'Version' ) );
+		wp_enqueue_script( 'jquery' );
 	wp_enqueue_script( 'popper-scripts', get_template_directory_uri() . '/js/popper.min.js', array(), false);
-    wp_enqueue_script( 'child-understrap-scripts', get_stylesheet_directory_uri() . '/js/app.min.js', array(), $the_theme->get( 'Version' ), true );
+		wp_enqueue_script( 'child-understrap-scripts', get_stylesheet_directory_uri() . '/js/app.min.js', array(), $the_theme->get( 'Version' ), true );
 }
 
 /* ADD FAVICON CONTENT
@@ -59,3 +59,13 @@ function theme_enqueue_styles() {
 // return get_bloginfo( 'title' );
 // }
 // add_filter( 'login_headertitle', 'aw_login_logo_url_title' );
+
+
+/* ADD ADVANCED CUSTOM FIELDS OPTIONS PAGE
+================================================== */
+
+// if( function_exists('acf_add_options_page') ) {
+
+//     acf_add_options_page();
+		
+// }
